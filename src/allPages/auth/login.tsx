069f19e281778f044357ca9bd3a0647de3bcd6c9
ali salehinia <alisalehinia79@gmail.com>
2025-07-06 18:31:04 +0330

    add animation to Card login instead of layout

--- src/allPages/auth/login.tsx
+++ src/allPages/auth/login.tsx
@@ -14,12 +14,15 @@ import { Label } from "@/components/ui/label"
 
 import { toast } from "sonner"
 
-export default function Login() {
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
+export default function Login() {
+  const pathname = usePathname();
   
   return (
     <div className="w-full h-screen 
-    flex justify-center 
+    flex justify-center items-center
    relative
     max-w-screen
     overflow-x-hidden
@@ -28,51 +31,62 @@ export default function Login() {
       <div className="absolute border-2 top-[30%] right-[20%] h-[120px] w-[120px] rounded-2xl hidden md:block rotate-12 shadow-lg -z-10" />
       <div className="absolute border-2 top-[50%] right-[50%] h-[300px] w-[300px] rounded-2xl hidden md:block rotate-6 shadow-lg -z-10" />
       <div className="absolute border-2 top-[0%] right-[80%] h-[400px] w-[400px] rounded-2xl hidden md:block rotate-45 shadow-lg -z-10" />
-      <Card className="w-full max-w-sm m-auto shadow-2xl ">
-        <CardHeader>
-          <CardTitle>ورود</CardTitle>
-          <CardDescription>
-            برای ورود به حساب کاربری شماره موبایل خود را وارد کنید.
-          </CardDescription>
-          <CardAction>
-            <Button variant="link">ثبت نام</Button>
-          </CardAction>
-        </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="phone">شماره موبایل</Label>
-                <Input
-                  id="phone"
-                  type="phone"
-                  placeholder="...09"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">رمز عبور</Label>
-                  <a
-                    href="#"
-                    className="mr-auto inline-block text-sm underline-offset-4 hover:underline"
-                  >
-                    فراموشی؟                </a>
+      <AnimatePresence mode="wait">
+        <motion.div
+          key={pathname}
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ duration: 0.3 }}
+          className="w-full"
+        >
+          <Card className="w-full max-w-sm m-auto shadow-2xl ">
+            <CardHeader>
+              <CardTitle>ورود</CardTitle>
+              <CardDescription>
+                برای ورود به حساب کاربری شماره موبایل خود را وارد کنید.
+              </CardDescription>
+              <CardAction>
+                <Button variant="link">ثبت نام</Button>
+              </CardAction>
+            </CardHeader>
+            <CardContent>
+              <form>
+                <div className="flex flex-col gap-6">
+                  <div className="grid gap-2">
+                    <Label htmlFor="phone">شماره موبایل</Label>
+                    <Input
+                      id="phone"
+                      type="phone"
+                      placeholder="...09"
+                      required
+                    />
+                  </div>
+                  <div className="grid gap-2">
+                    <div className="flex items-center">
+                      <Label htmlFor="password">رمز عبور</Label>
+                      <a
+                        href="#"
+                        className="mr-auto inline-block text-sm underline-offset-4 hover:underline"
+                      >
+                        فراموشی؟                </a>
+                    </div>
+                    <Input id="password" type="password" required />
+                  </div>
                 </div>
-                <Input id="password" type="password" required />
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full" onClick={()=>{
-            toast("عملیات با موفقیت انجام شد")
-          }}>
-            ورود
-          </Button>
-          
-        </CardFooter>
-      </Card>
+              </form>
+            </CardContent>
+            <CardFooter className="flex-col gap-2">
+              <Button type="submit" className="w-full" onClick={()=>{
+                toast("عملیات با موفقیت انجام شد")
+              }}>
+                ورود
+              </Button>
+              
+            </CardFooter>
+          </Card>
+        </motion.div>
+      </AnimatePresence>
     </div>
   )
 }
